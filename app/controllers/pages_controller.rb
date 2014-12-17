5181f04731d6e0df84683bcc3d0b0b4c28bcb557
Elliott Abernathy <elliott_abernathy@yahoo.com>
2014-12-17 17:54:33 +0000

    First Commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def About
+  end
+
+  def Contact
+  end
+
+  def Photography
+  end
+
+  def Web_Templates
+  end
+
+  def Music
+  end
+
+  def Fashion
+  end
+end
